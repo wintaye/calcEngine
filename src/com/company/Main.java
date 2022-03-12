53806efb496867e2ee612af9d6fc96471669cb47
Wintaye Gebru <wintayeg@gmail.com>
2022-03-11 18:22:24 -0600

    calcEngine pluralsight app - switch statement, relational operator

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,24 +4,29 @@ public class Main {
 
     public static void main(String[] args) {
         double value1 = 100.0d;
-        double value2 = 0.0d;
+        double value2 = 50.0d;
         double result = 0.0d;
 
         char opCode = 'd';
 
-        if(opCode == 'a')
-            result = value1 + value2;
-        else if(opCode == 's')
-            result = value2 - value1;
-        else if(opCode == 'm')
-            result = value1 * value2;
-        else if(opCode == 'd') {
-            if (value2 != 0)
-                result = value1 / value2;
-        }
-        else {
-            System.out.println("Invalid OpCode: " + opCode);
-            result = 0.0d;
+        switch(opCode) {
+            case 'a':
+                result = value1 + value2;
+                break;
+            case 's':
+                result = value2 - value1;
+                break;
+            case 'm':
+                result = value1 * value2;
+                break;
+            case 'd':
+                result = value2 != 0 ? value1 / value2 : 0.0d;
+                break;
+
+            default:
+                System.out.println("Invalid OpCode: " + opCode);
+                result = 0.0d;
+                break;
         }
             System.out.println(result);
 
