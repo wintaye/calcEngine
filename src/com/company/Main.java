0259a3b030ed11f050064dd575138bb98de69129
Wintaye Gebru <wintayeg@gmail.com>
2022-03-11 19:13:27 -0600

    calcEngine pluralsight app - for, for each, arrays

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,32 +3,34 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        double value1 = 100.0d;
-        double value2 = 50.0d;
-        double result = 0.0d;
 
-        char opCode = 'd';
+        double [] leftVals = {100.0d, 25.0d, 225.0d, 11.0d};
+        double [] rightVals = {50.0d, 92.0d, 17.0d, 3.0d};
+        char [] opCode = {'d', 'a', 's', 'm'};
+        double[] results = new double[opCode.length];
 
-        switch(opCode) {
-            case 'a':
-                result = value1 + value2;
-                break;
-            case 's':
-                result = value2 - value1;
-                break;
-            case 'm':
-                result = value1 * value2;
-                break;
-            case 'd':
-                result = value2 != 0 ? value1 / value2 : 0.0d;
-                break;
-
-            default:
-                System.out.println("Invalid OpCode: " + opCode);
-                result = 0.0d;
-                break;
+        for(int i = 0; i < opCode.length; i++) {
+            switch (opCode[i]) {
+                case 'a':
+                    results[i] = leftVals[i] + rightVals[i];
+                    break;
+                case 's':
+                    results[i] = leftVals[i] - rightVals[i];
+                    break;
+                case 'm':
+                    results[i] = leftVals[i] * rightVals[i];
+                    break;
+                case 'd':
+                    results[i] = rightVals[i] != 0 ? leftVals[i] / rightVals[i] : 0.0d;
+                    break;
+                default:
+                    System.out.println("Invalid OpCode: " + opCode[i]);
+                    results[i] = 0.0d;
+                    break;
+            }
         }
-            System.out.println(result);
+            for(double allResults : results)
+            System.out.println(allResults);
 
     }
 }
